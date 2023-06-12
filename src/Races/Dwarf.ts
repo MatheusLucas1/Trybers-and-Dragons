8bef7892deea4157fce1da4e14cde91787f635b1
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-06-12 09:11:21 -0300

    requisito 2

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _instance = 0;
+
+  constructor(name: string, dexterity: number) { 
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._instance += 1;
+  }
+
+  override get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._instance;
+  }
+}
+
+export default Dwarf;
